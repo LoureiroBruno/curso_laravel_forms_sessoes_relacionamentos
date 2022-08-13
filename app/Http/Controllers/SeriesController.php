93ab1ec547efc2ffae8096fa20539a81160e9015
LoureiroBruno <brunoloureiro9@yahoo.com.br>
2022-08-13 15:25:39 -0300

    session flah messages

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -12,11 +12,17 @@ class SeriesController extends Controller
      *
      * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
         // $series = Serie::all();
         $series = Serie::orderBy('nome', 'asc')->get();
-        return view('series.index')->with('series', $series);
+
+         /** recebido  do metodo destroy*/
+        $mensagemDestroy = $request->session()->get('mensagem.destroy');
+        $request->session()->forget('mensagem.destroy');
+
+        return view('series.index')->with('series', $series)->with('mensagemDestroy', $mensagemDestroy);
+
     }
 
     /**
@@ -40,9 +46,11 @@ class SeriesController extends Controller
         $nomeSerie = $request->input('nome');
         if ($nomeSerie != null) {
             Serie::create($request->all());
-            return to_route('series.index')->with('success', 'Cadastro Realizado com Sucesso!');
+
+            return to_route('series.index')->with("success", "Cadastrado a série ({$nomeSerie}) com sucesso!");
         } else {
-            return redirect('series')->with('warning', 'Preencher título da série campo nome');
+
+            return redirect('series')->with("danger", "Há dados não informados, tente novamente!");
         }
     }
 
@@ -51,9 +59,15 @@ class SeriesController extends Controller
         $idSerie = $request->series;
         if ($request->series != null) {
             Serie::destroy($request->series);
-            return to_route('series.index')->with('success', "Excluído o Registro de n° {$idSerie} com Sucesso! ");
+            $request->session()->put('mensagem.destroy', "Excluído o Registro de n° {$idSerie} com Sucesso!");
+
+            /** enviado para a rota index que invia a view index */
+            return to_route('series.index');
         } else {
-            return to_route('series.index')->with('danger', "Nenhum registro foi encontrado!");
+            /** caso use session->flash desnecessário uso session()->forget() */
+            $request->session()->put('mensagem.destroy', "Nenhuma identificação no registro foi encontrado!");
+
+            return to_route('series.index');
         }
     }
 
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -10,24 +10,19 @@
                 <div class="alert alert-success">
                     {{ session('success') }}
                 </div>
-            @elseif (session('warning'))
-                <div class="alert alert-warning">
-                    {{ session('warning') }}
-                </div>
             @elseif (session('danger'))
                 <div class="alert alert-danger">
                     {{ session('danger') }}
                 </div>
             @endif
 
-            {{-- @if(session('success') || session('warning') || session('danger'))
-                <div class="alert alert-{{ session('success') }} {{ session('warning') }} {{ session('danger') }}">
-                    {{ session('success') }} {{ session('warning') }} {{ session('danger') }}
+            @isset($mensagemDestroy)
+                <div class="alert alert-info">
+                    {{ $mensagemDestroy }}
                 </div>
-            @endif --}}
+            @endisset
 
-
-            <a href="{{ route('series.create') }}" class="btn btn-primary mb-4" tabindex="-1" role="button"
+            <a href="{{ route('series.create') }}" class="btn btn-primary btn-sm mb-4" tabindex="-1" role="button"
                 aria-disabled="true">Adicionar</a>
 
             <table class="table table-hover">
@@ -49,20 +44,20 @@
                             <td>{{ $serie->updated_at }}</td>
                             <td class="td-coluna-acoes-tabela-series">
                                 <div>
-
-                                    <form action="{{ route('series.destroy', $serie->id) }}" method="post" id="btn-destroy">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-outline-danger btn-sm mb-1">
-                                            Excluir
-                                        </button>
-                                    </form>
-
-                                    <form  action="" method="post" id="btn-update">
+                                    <form action="" method="post" id="btn-update">
                                         @csrf
                                         {{-- @method('EDIT') --}}
                                         <button type="submit" class="btn btn-outline-dark btn-sm mb-1">
                                             Editar
+                                        </button>
+                                    </form>
+
+                                    <form action="{{ route('series.destroy', $serie->id) }}" method="post"
+                                        id="btn-destroy">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-outline-danger btn-sm mb-1">
+                                            Excluir
                                         </button>
                                     </form>
                                 </div>
